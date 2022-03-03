59efc3d959278bf087641f2d55351ab2b0f72e14
Ziya <ziyagunay06@gmail.com>
2022-03-03 20:26:19 +0100

    Third  Commit for this branch

--- src/Hat.java
+++ src/Hat.java
@@ -4,6 +4,8 @@ public class Hat {
         System.out.println("First Line of Code");
         //second line at this branch
         System.out.println("Second Line of Code");
+        // third commit
+        System.out.println("Third Line of Code");
 
     }
 }
