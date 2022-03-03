c2695308a2b28e432ed0b9be94a4a7a2f5042e62
Ziya <ziyagunay06@gmail.com>
2022-03-03 20:34:59 +0100

    Second  Commit at C branch

--- src/Store.java
+++ src/Store.java
@@ -2,6 +2,8 @@ public class Store {
     public static void main(String[] args) {
         // first
         System.out.println("First line of Code");
+        // second
+        System.out.println("Second");
 
     }
 }
